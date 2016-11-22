24b047ded13f1d92d44857b1f640adefe798553e
David Inga <david.inga@vizzuality.com>
2016-11-22 15:34:40 +0100

    removing debug

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,7 +1,8 @@
-const debug = require('debug')('half-earth');
 const ghpages = require('gh-pages');
 const path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), (err) => {
-  debug('Error: %s', err);
+ghpages.publish(path.join(__dirname, '../dist'), (err) => {
+  if (err) {
+    console.error('Error: %s', err);
+  }
 });
