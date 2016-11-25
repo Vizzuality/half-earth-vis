2f1bec5b8315613f925d1197fa9e519914490651
Rodrigo Solís <rodrigo.solis@vizzuality.com>
2016-11-25 16:32:57 +0100

    fixes issue with map

--- src/app.js
+++ src/app.js
@@ -83,13 +83,15 @@ function onReady() {
 
     // Section 4: map in orthographic
     if (y < (h * 2)) {
-      let introHeading = document.getElementById(selector);
+      let introHeading = document.getElementById('intro-heading');
       introHeading.style.filter = `blur(${y / 20}px)`;
 
-      let scrollAnimation = document.getElementById('scroll-animation')
+      let scrollAnimation = document.getElementById('scroll-down-animation')
       scrollAnimation.classList.add('-hidden');
 
-    } else if (y >= (h * 2) && y < (h * 5)) {
+      app.world.setProjection('robinson');
+
+    } else if (y >= (h * 2) && y < (h * 8)) {
       app.world.setProjection('orthographic');
     } else {
       app.world.setProjection('robinson');
