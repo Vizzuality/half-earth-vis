4b2715ab52910c15a39e1a855d56f8660436522e
David Inga <david.inga@vizzuality.com>
2016-11-30 12:11:17 +0100

    using vizz-component

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,9 @@ import './styles/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Globe} from 'vizz-components';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import Globe from './components/Globe';
 import Story from './components/Story';
 
 class AppComponent extends React.Component {
