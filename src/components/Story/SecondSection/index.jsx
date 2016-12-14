8fe5486201ad535db73f0d89c58358c6db317e23
Hector Arce de las Heras <hector.arce@iMac-de-Hector.local>
2016-12-14 20:07:32 +0100

    improve second section with texts and new styles

--- src/components/Story/SecondSection/index.jsx
+++ src/components/Story/SecondSection/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TitleSection from '../TitleSection';
 import './style.scss';
 import firstImage from './assets/road.jpg';
 import secondImage from './assets/tigers.jpg';
@@ -16,7 +17,20 @@ class SecondSection extends React.Component{
           <img className="second-image" src={secondImage} alt="Road" />
           <img className="fourth-image" src={fourthImage} alt="Road" />
           <img className="sixth-image" src={sixthImage} alt="Road" />
+          <div className="text-section first">
+            <p>Suitable habitat, crucial to nature’s survival,</p>
+            <p>is shrinking fast. In fact the rate of</p>
+            <p>extinction today is at least 1,000 times</p>
+            <p>higher than at any time in Earth’s history.</p>
+          </div>
+
+          <div className="text-section second">
+            <TitleSection text="We must acT NOW" />
+            <p>Before we’ve lost irreplaceable species</p>
+            <p>and ecosystems forever.</p>
+          </div>
         </div>
+
         <div className="large-image">
           <div className="third-image" alt="Road">{''}</div>
         </div>
