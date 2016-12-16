826da21951f19381c9e0e2bb77ee331c0a2df29e
Hector Arce de las Heras <hector.arce@iMac-de-Hector.local>
2016-12-16 12:50:09 +0100

    update styles and content for video component

--- src/components/Story/VideoSection/index.jsx
+++ src/components/Story/VideoSection/index.jsx
@@ -7,27 +7,10 @@ class VideoSection extends React.Component{
 
   // IMPROVE CODE
 
-  openVideo(e) {
-    $('.play-button').css('display', 'none');
-    $('.video-player').toggleClass('-opened');
-    $('.backdark').toggleClass('-opened');
-    setTimeout(function(){
-      $('.video-player')[0].play();
-    }, 1100);
-  }
-
-  closeVideo(e) {
-    $('.video-player').toggleClass('-opened');
-    $('.backdark').toggleClass('-opened');
-  }
-
   render() {
     return (
       <div className="c-video-section">
-        <span className="play-button" onClick={(e) => this.openVideo(e)}>Play video</span>
-        {/*<span className="close-video-button" onClick={(e) => this.closeVideo(e)}>X</span>*/}
-        <div className="backdark">{''}</div>
-        <video className="video-player">
+        <video className="video-player" autoPlay loop>
           <source src="src/components/Story/SeventhSection/assets/demo.mp4" type="video/mp4"></source>
           Your browser does not support the video tag.
         </video>
